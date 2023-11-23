0c3d88fce75395df1f95858e0f7d229690c8671c
DiegoLudena <d_l_m88@yahoo.es>
2023-11-23 12:22:54 +0100

    Inicio proyecto

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+    	//Indico la ruta de la vista
+        Parent root = FXMLLoader.load(getClass().getResource("Vista/DICaso1.fxml"));
+        //Creo la escena
+    	primaryStage.setTitle("Caso Práctico 1");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+    }
+
+    //el programa lanza la escena
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
